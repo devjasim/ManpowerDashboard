82d54eb0b06cbd0b0b238f344bc4c7b5e553568b
Jasim Uddin <jasim.jsr360@gmail.com>
2019-11-05 17:46:07 +0600

    final

--- ManPower/assets/js/main.js
+++ ManPower/assets/js/main.js
@@ -32,3 +32,9 @@ $(document).ready(function(){
     });
 });
 
+$(function () {
+	$("#datepicker").datepicker({ 
+		  autoclose: true, 
+		  todayHighlight: true
+	}).datepicker('update', new Date());
+});
